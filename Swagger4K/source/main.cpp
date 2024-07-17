278eecca8b26d0c64b81e5e84a43cae4d619721b
Ty [Indev] <168133165+yophlox@users.noreply.github.com>
2024-07-16 22:35:28 -0400

    strums

--- Swagger4K/source/main.cpp
+++ Swagger4K/source/main.cpp
@@ -1,92 +1,107 @@
-#include <iostream>
 #include <SDL.h>
-#undef main
-#include <stdio.h>
+#include <vector>
 
-using namespace std;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const int STRUM_NOTE_WIDTH = 50;
+const int STRUM_NOTE_HEIGHT = 20;
+const int SPACING = 20; 
+const int VERTICAL_OFFSET = 50;
+const int PRESS_ANIM_OFFSET = 5; 
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
+struct Note {
+    int x, y;
+    bool pressed;
+};
 
-bool init();
-bool loadMedia();
-void close();
-SDL_Window* gWindow = NULL;
-SDL_Surface* gScreenSurface = NULL;
-SDL_Surface* gSwaggerMain = NULL;
+void drawStrumNotes(SDL_Renderer* renderer, std::vector<Note>& notes) {
+    for (auto& note : notes) {
+        SDL_Rect rect;
+        rect.x = note.x;
+        rect.y = note.pressed ? note.y + PRESS_ANIM_OFFSET : note.y;
+        rect.w = STRUM_NOTE_WIDTH;
+        rect.h = STRUM_NOTE_HEIGHT;
 
-bool init()
-{
-	bool success = true;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		gWindow = SDL_CreateWindow("Swagger 4K", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			gScreenSurface = SDL_GetWindowSurface(gWindow);
-		}
-	}
-
-	return success;
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+    }
 }
 
-bool loadMedia()
-{
-	bool success = true;
+int main(int argc, char* args[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL could not initialize! SDL_Error: %s", SDL_GetError());
+        return -1;
+    }
 
-	gSwaggerMain = SDL_LoadBMP("assets/images/test/bf.bmp");
-	if (gSwaggerMain == NULL)
-	{
-		printf("Unable to load image %s! SDL Error: %s\n", "assets/images/test/bf.bmp", SDL_GetError());
-		success = false;
-	}
+    SDL_Window* window = SDL_CreateWindow("Swagger 4K",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        SDL_Log("Window could not be created! SDL_Error: %s", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
 
-	return success;
-}
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Renderer could not be created! SDL_Error: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-void close()
-{
-	SDL_FreeSurface(gSwaggerMain);
-	gSwaggerMain = NULL;
+    std::vector<Note> notes(4);
+    bool downscroll = false;
 
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
+    int y_position = downscroll ? (SCREEN_HEIGHT - STRUM_NOTE_HEIGHT - VERTICAL_OFFSET) : VERTICAL_OFFSET;
+    for (int i = 0; i < notes.size(); ++i) {
+        notes[i] = {
+            static_cast<int>((SCREEN_WIDTH - (notes.size() * (STRUM_NOTE_WIDTH + SPACING) - SPACING)) / 2 + i * (STRUM_NOTE_WIDTH + SPACING)),
+            y_position,
+            false
+        };
+    }
 
-	SDL_Quit();
-}
+    bool quit = false;
+    SDL_Event e;
 
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN) {
+                switch (e.key.keysym.sym) {
+                case SDLK_a: notes[0].pressed = true; break;
+                case SDLK_s: notes[1].pressed = true; break;
+                case SDLK_k: notes[2].pressed = true; break;
+                case SDLK_l: notes[3].pressed = true; break;
+                }
+            }
+            if (e.type == SDL_KEYUP) {
+                switch (e.key.keysym.sym) {
+                case SDLK_a: notes[0].pressed = false; break;
+                case SDLK_s: notes[1].pressed = false; break;
+                case SDLK_k: notes[2].pressed = false; break;
+                case SDLK_l: notes[3].pressed = false; break;
+                }
+            }
+        }
 
-int main() {
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
-	}
-	else
-	{
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			SDL_BlitSurface(gSwaggerMain, NULL, gScreenSurface, NULL);
-			SDL_UpdateWindowSurface(gWindow);
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-		}
-	}
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
 
-	close();
+        drawStrumNotes(renderer, notes);
 
-	return 0;
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
